bc3e5f07a931ed9710f52c6a427b6a8e274310b0
Dahveyea <dahveyea.cowan@gmail.com>
2019-11-07 19:35:45 -0800

    Updated app.py

--- app.py
+++ app.py
@@ -1,26 +1,30 @@
-import requests
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/fortune')
+@app.route('/')
 def index():
+    return render_template('index.html')
+
+
+@app.route('/fortune')
+def fortune_form():
     return render_template('fortune_form.html')
 
 
 @app.route('/fortune_results')
 def fortune_results():
     """Displays the user's fortune."""
-    users_favorite_animal = requests.args.get('Naruto')
+    anime = request.args.get("anime")
     # ... etc
-
-    if users_favorite_animal == 'unicorn':
-        print ("You'll have a magical day!")
-    elif users_favorite_animal == 'bear':
-        print("Your future is fish")
-    elif users_favorite_animal == 'toad':
-        print("Something sticky")
+    fortune = " "
+    if anime == 'Naruto':
+        fortune = "You'll have a magical day!"
+    elif anime == 'One Piece':
+        fortune = "Your super power will be Teleportation"
+    elif anime == 'Angel Beats':
+        fortune = "You will be very rich"
     else:
-        print("no other fortune applies, return default fortune")
-    return render_template('fortune_results.html')
+        fortune = "no other fortune applies, return default fortune"
+    return render_template('fortune_results.html', fortune=fortune)
